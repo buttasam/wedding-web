6b8dc8cbe75db1ef4788abbd5d667ab45502b1eb
samuel.butta <samuel.butta@firma.seznam.cz>
2021-03-21 14:28:57 +0100

    Do not remove CNAME

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,4 +3,7 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
+sh.rm('-rf', `${destPath}/assets`)
+sh.rm('-rf', `${destPath}/css`)
+sh.rm('-rf', `${destPath}/js`)
+sh.rm('-rf', `${destPath}/index.html`)
